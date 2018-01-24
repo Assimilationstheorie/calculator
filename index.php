8842f617d76913ecbbfeb86284f6c267b35e6f19
Konstantin Schmidt <konstantin@leafhouse.de>
2018-01-24 12:35:26 +0100

    optimize

--- index.php
+++ index.php
@@ -4,39 +4,45 @@ class Taschenrechner {
   protected $zahl;
   protected $final_number;
   protected $zahlen = [
-      "eins" => 1,
-      "ein" => 1,
-      "zwei" => 2,
-      "drei" => 3,
-      "vier" => 4,
-      "fünf" => 5,
-      "sechs" => 6,
-      "sieben" => 7,
-      "acht" => 8,
-      "neun" => 9,
-      "zehn" => 10,
-      "elf" => 11,
-      "zwölf" => 12,
-      "dreizehn" => 13,
-      "vierzehn" => 14,
-      "fünfzehn" => 15,
-      "sechszehn" => 16,
-      "siebzehn" => 17,
-      "achtzehn" => 18,
-      "neunzehn" => 19,
-      "zwanzig" => 20,
-      "dreißig" => 30,
-      "vierzig" => 40,
-      "fünfzig" => 50,
-      "sechszig" => 60,
-      "siebzig" => 70,
-      "achtzig" => 80,
-      "neunzig" => 90,
-      "hundert" => 100,
-      "einhundert" => 100,
-      "tausend" => 1000,
-      "eintausend" => 1000,
-      "hunderttausend" => 100000
+    "eins" => 1,
+    "ein" => 1,
+    "zwei" => 2,
+    "drei" => 3,
+    "vier" => 4,
+    "fünf" => 5,
+    "sechs" => 6,
+    "sieben" => 7,
+    "acht" => 8,
+    "neun" => 9,
+    "zehn" => 10,
+    "elf" => 11,
+    "zwölf" => 12,
+    "dreizehn" => 13,
+    "vierzehn" => 14,
+    "fünfzehn" => 15,
+    "sechszehn" => 16,
+    "siebzehn" => 17,
+    "achtzehn" => 18,
+    "neunzehn" => 19,
+    "zwanzig" => 20,
+    "dreißig" => 30,
+    "vierzig" => 40,
+    "fünfzig" => 50,
+    "sechszig" => 60,
+    "siebzig" => 70,
+    "achtzig" => 80,
+    "neunzig" => 90,
+    "hundert" => 100,
+    "einhundert" => 100,
+    "tausend" => 1000,
+    "eintausend" => 1000,
+    "hunderttausend" => 100000
+   ];
+
+   protected $key_words = [
+     'tausend',
+     'hundert',
+     'und'
    ];
 
   public function translate($zahl) {
@@ -56,21 +62,17 @@ class Taschenrechner {
         $this->final_number = $this->zahlen[$this->zahl];
       } else {
         //Alle Sonderfälle
-        $this->final_number = $this->butcher();
+            $this->final_number = $this->butcher();
       }
     }
     return $this->final_number;
-}
+  }
 
   private function butcher() {
-    //Tausend kommt nur einmal vor
-    //Hundert 2x vorkommen kann
-    //Und kann 2x vorkommen
-    $key_words = ['tausend', 'hundert', 'und'];
     //Prüft, ob es das Wort "Tausend" gibt
-    if(preg_match("/$key_words[0]/", $this->zahl)) {
+    if(preg_match("/".$this->key_words[0]."/", $this->zahl)) {
       //String wird in zwei Teile geteilt - Das Wort "Tausend" ist hierbei der Trenner
-      $tausend = explode($key_words[0], $this->zahl);
+      $tausend = explode($this->key_words[0], $this->zahl);
       //Prüfen, ob hinter "tausend" noch etwas ist
       if(empty($tausend[1])) {
         //Hinter Tausend befindet sich nichts mehr!!!!!!!!
@@ -87,45 +89,43 @@ class Taschenrechner {
           $this->final_number = ($this->detective($tausend[0]) * $this->zahlen['tausend']) + $this->detective($tausend[1]) ;
       }
       //Tausend nicht gefunden
-  } else {
-    $key_words = ['hundert', 'und'];
-    //Schaut ob das Wort "hundert" drin ist
-    if(preg_match("/$key_words[0]/", $this->zahl)) {
-      $hundert = explode($key_words[0], $this->zahl);
-      //ist der letzte teil leer?
-      if(empty($hundert[1])) {
-        $this->final_number = $this->zahlen[$hundert[0]] * $this->zahlen['hundert'];
-      } else {
-        if(preg_match("/$key_words[1]/", $hundert[1])) {
-          $und = explode($key_words[1], $hundert[1]);
-          $this->final_number = ($this->zahlen[$hundert[0]] * $this->zahlen['hundert']) + $this->zahlen[$und[0]] + $this->zahlen[$und[1]];
+    } else {
+      //Schaut ob das Wort "hundert" drin ist
+      if(preg_match("/".$this->key_words[1]."/", $this->zahl)) {
+        $hundert = explode($this->key_words[1], $this->zahl);
+        //ist der letzte teil leer?
+        if(empty($hundert[1])) {
+          $this->final_number = $this->zahlen[$hundert[0]] * $this->zahlen['hundert'];
         } else {
-          $this->final_number = ($this->zahlen[$hundert[0]] * $this->zahlen['hundert']) + $this->zahlen[$hundert[1]];
+          if(preg_match("/".$this->key_words[2]."/", $hundert[1])) {
+            $und = explode($this->key_words[2], $hundert[1]);
+            $this->final_number = ($this->zahlen[$hundert[0]] * $this->zahlen['hundert']) + $this->zahlen[$und[0]] + $this->zahlen[$und[1]];
+          } else {
+            $this->final_number = ($this->zahlen[$hundert[0]] * $this->zahlen['hundert']) + $this->zahlen[$hundert[1]];
+          }
+        }
+      } else {
+        if(preg_match("/".$this->key_words[2]."/", $this->zahl)) {
+          $und = explode($this->key_words[2], $this->zahl);
+          $this->final_number = $this->zahlen[$und[0]] + $this->zahlen[$und[1]];
         }
       }
-    } else {
-      if(preg_match("/und/", $this->zahl)) {
-        $und = explode("und", $this->zahl);
-        $this->final_number = $this->zahlen[$und[0]] + $this->zahlen[$und[1]];
-      }
     }
+    return $this->final_number;
   }
-  return $this->final_number;
-}
 
   //Irgendwelche Zahlen interpretieren
   private function detective($zahl) {
-    $key_words = ['hundert', 'und'];
     //Schaut ob das Wort "hundert" drin ist IM ERSTEN TEIL vor TAUSEND
-    if(preg_match("/$key_words[0]/", $zahl)) {
+    if(preg_match("/".$this->key_words[1]."/", $zahl)) {
       //Schneidet alles nach "hundert" - Nimmt den ersten Teil
-      $hundert = explode($key_words[0], $zahl);
+      $hundert = explode($this->key_words[1], $zahl);
       //Nimm die Zahl vor dem Wort "hundert" und nimmt sie mal 100
       $vorHundert = $this->zahlen[$hundert[0]] * $this->zahlen['hundert'];
       //Hinter hundert und vor tausend - die zahl - Schaut, ob das Wort ein "und" hat
-      if(preg_match("/$key_words[1]/", $hundert[1])) {
+      if(preg_match("/".$this->key_words[2]."/", $hundert[1])) {
         //Das Und wird abgetrennt
-        $und = explode($key_words[1], $hundert[1]);
+        $und = explode($this->key_words[2], $hundert[1]);
         //Setzen die erste Zahl nach "hundert" und addiere die beiden Zahlen zwischen "und" - vor tausend
         $final = $vorHundert + $this->zahlen[$und[1]] + $this->zahlen[$und[0]];
       } else {
@@ -137,9 +137,9 @@ class Taschenrechner {
         }
       }
     } else {
-      if(preg_match("/$key_words[1]/", $zahl)) {
+      if(preg_match("/".$this->key_words[2]."/", $zahl)) {
         //Das Und wird abgetrennt
-        $und = explode($key_words[1], $zahl);
+        $und = explode($this->key_words[2], $zahl);
         //Setzen die erste Zahl nach "hundert" und addiere die beiden Zahlen zwischen "und" - vor tausend
         $final = $this->zahlen[$und[1]] + $this->zahlen[$und[0]];
       } else {
@@ -152,10 +152,7 @@ class Taschenrechner {
 }
 
 $service = new Taschenrechner();
-//525386
-//Dreihunder geht nicht
-$zahl = "fünfhundertfünfundzwanzigtausenddreihundertsechsundachtzig";
- // $zahl = 2000;
+$zahl = "fünfhundert";
 
 echo "<hr>";
 echo "<pre>";
